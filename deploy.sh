6257fa1a6f245b3a28d6ca10c726b3b58df2ed20
neo0910 <denikkalenik@gmail.com>
2019-06-29 11:57:25 +0300

    NSC-9 deploy setup

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-npm run build
-
 cd dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:neo0910/neo0910.github.io.git master
 
 cd -
+
+rm -r -f dist
